e34eb7e6c8f47bad6e5254162a1c430c31c619b6
sajibchandraroy <66843005+sajibchandraroy@users.noreply.github.com>
2021-01-06 01:29:20 +0600

    private route applied easily

--- routes/shops.js
+++ routes/shops.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const shopController = require('../controllers/shop.controller');
 const userController = require('../controllers/user.controller');
 
-/* GET users listing. */
-router.post('/', userController.isAuthenticated, shopController.create);
+
 router.get('/', shopController.getAll);
 router.get('/:id', shopController.getById);
-router.put('/:id', userController.isAuthenticated, shopController.updateById);
-router.delete('/:id', userController.isAuthenticated, shopController.deleteById);
+router.all('/*', userController.isAuthenticated);
+
+/* GET users listing. */
+router.post('/', shopController.create);
+// router.get('/', shopController.getAll);
+// router.get('/:id', shopController.getById);
+router.put('/:id', shopController.updateById);
+router.delete('/:id', shopController.deleteById);
 
 module.exports = router;
